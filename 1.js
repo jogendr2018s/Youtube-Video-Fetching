53327485cdc69c778dba1c392d314d152898c5dd
jogendr2018s <jogendraruppa@gmail.com>
2024-03-14 15:42:16 +0530

    first commit

---
+++ 1.js
@@ -0,0 +1,31 @@
+const youtubesearchapi = require("youtube-search-api");
+const express = require("express")
+const app = express()
+app.use(express.json());
+const cors = require('cors');
+
+async function hello(text) {
+    try {
+        const a = await youtubesearchapi.GetListByKeyword(`${text}`, [false], [3], [{type: "video"}]);
+        return a;
+    } catch (error) {
+        console.error("Error in fetching YouTube data:", error);
+        return { error: "Failed to fetch YouTube data" };
+    }
+}
+
+app.get('/', async (req, res) => {
+    try {
+        console.log(req.query.text);
+        const jsonData = await hello(req.query.text);
+        console.log(jsonData);
+        res.json(jsonData);
+    } catch (error) {
+        console.error("Error in processing request:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+app.listen(8000, () => {
+    console.log('connected.....');
+});
